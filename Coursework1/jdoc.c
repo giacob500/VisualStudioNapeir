a43cd8a1676f0089722b1c17cea665549aed64f4
giacob500 <giacob500@gmail.com>
2021-02-26 15:31:00 +0100

    Coursework 4.3.1

--- Coursework1/jdoc.c
+++ Coursework1/jdoc.c
@@ -32,38 +32,59 @@ void writeOutputFile(char* inputFile, char* outputFile) {
 }
 
 void promptInfos(char* inputFile) {
+	// flags
 	int saveNextToken = 0;
-	char info[100];
-	memset(info, 0, 100); // CAN I DO THIS?
-	char name[100];
+	int author = 0;
+	int param = 0;
+	int retrn = 0;
+
+
+	char info[100] = "";
+	
+	//memset(info, 0, 100);	//<--------------------------------- CAN I DO THIS?
+	printf("%s\n", info);
 	FILE* javaFile = fopen(inputFile, "r");
 	char line[1000];
-	char lineCopy[1000];
+	printf("\n");
 	while (fgets(line, 1000, javaFile) != NULL) {
-		strcpy(lineCopy, line);
-		char* delimiters = " .,\n";
+		char* delimiters = " .,;\n";
 		char* token = strtok(line, delimiters);
 		// Loop to pass through the single words
 		while (token != NULL) {
 			if (!strcmp(token, "*/")) {
-				strcat(info, "\n");
+				strcat(info, "\n\n");
 				saveNextToken = 0;			
 			}
 			if (saveNextToken == 1) {
+				if (author == 1) {
+					strcat(info, "Author: ");
+					author = 0;
+				}
+				else if (param == 1) {
+					strcat(info, "Parameter: ");
+					param = 0;
+				}
+				else if (retrn == 1) {
+					strcat(info, "Returns: ");
+					retrn = 0;
+				}
 				strcat(info, token);
 				strcat(info, " ");
 			}
 			if (!strcmp(token, "@author")) {
 				saveNextToken = 1;
+				author = 1;
 			}
 			if (!strcmp(token, "@param")) {
 				saveNextToken = 1;
+				param = 1;
 			}
 			if (!strcmp(token, "@return")) {
 				saveNextToken = 1;
+				retrn = 1;
 			}
 			token = strtok(NULL, delimiters);			
-		}		
+		}
 	}	
 	printf("%s\n", info);
 	fclose(javaFile);
