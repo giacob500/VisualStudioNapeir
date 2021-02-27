61e67a2e1ccf8da6c29d673f9f0896883771be08
giacob500 <giacob500@gmail.com>
2021-02-27 21:15:37 +0100

    Coursework 5.0: GOD EXISTS

--- Coursework1/jdoc.c
+++ Coursework1/jdoc.c
@@ -38,12 +38,26 @@ void promptInfos(char* inputFile) {
 	int param = 0;
 	int retrn = 0;
 	int clas = 0;
-
+	char objects[10][10] = {
+						 {'i', 'n', 't', '\0'},					
+						 {'c', 'h', 'a', 'r', '\0'},			
+						 {'f', 'l', 'o', 'a', 't', '\0'},		
+						 {'d', 'o', 'u', 'b', 'l', 'e','\0'},	
+						 {'b', 'y', 't', 'e', '\0'},				
+						 {'s', 'h', 'o', 'r', 't', '\0'},		
+						 {'l', 'o', 'n', 'g', '\0'},			
+						 {'v', 'o', 'i', 'd', '\0'},
+						 {'b', 'o', 'o', 'l', 'e', 'a', 'n','\0'},
+						 {'S', 't', 'r', 'i', 'n', 'g','\0'}
+	};
+	int arraySize = 10;
+	int possibleMethod = 0;
+	int method = 0;
+	int wordsCounter = 0;
 
 	char info[100] = "";
 	char saveForLater[100] = "";
-	//memset(info, 0, 100);
-	printf("%s\n", info);
+	char saveOtherStuffForLater[100] = "";
 	FILE* javaFile = fopen(inputFile, "r");
 	char line[1000];
 	while (fgets(line, 1000, javaFile) != NULL) {
@@ -64,32 +78,57 @@ void promptInfos(char* inputFile) {
 			}
 			else if (clas == 1) {
 				printf("Class: %s\n", token);
-				printf("Author: %s\n", saveForLater);
+				printf("Author: %s", saveForLater);
+				strcpy(saveForLater, "");
+			}
 
+			else if (method == 1) {
+				printf("Method: %s\n", saveOtherStuffForLater);
+				if (param = 1) {
+					printf("Parameter: %s", saveForLater);					
+					param = 0;
+				}
+				else if (retrn = 1) {
+					printf("Returns: %s", saveForLater);
+					retrn = 0;
+				}
+				strcpy(saveForLater, "");
+				method = 0;
+			}
+			else if (possibleMethod == 1) {
+				strcpy(saveOtherStuffForLater, "");
+				strcpy(saveOtherStuffForLater, token);
+				possibleMethod = 0;
 			}
 
 			else if (saveNextToken == 1) {
-				printf("%s ", token);
+				strcat(saveForLater, token);
+				strcat(saveForLater, " ");
 			}
 			else if (!strcmp(token, "@return")) {
-				printf("Rtuerns: ");
 				saveNextToken = 1;
 			}
 			else if (!strcmp(token, "@param")) {
-				printf("Parameter: ");
 				saveNextToken = 1;
 			}
-
 			else if (!strcmp(token, "@author")) {
 				author = 1;
 			}
 			else if (!strcmp(token, "class")) {
 				clas = 1;
 			}
-			
+			else if (!strcmp(token, "(")) {
+				method = 1;
+			}
+
 			//check if it is a method
-
-
+			//qui ci va un for che scorre tutti gli elementi della lista con dentro l'if sotto
+			for (int i = 0; i < arraySize; ++i) {
+				if (!strcmp(token, (char*)objects[i]) && possibleMethod == 0) {
+					possibleMethod = 1;
+				}
+			}
+			//printf("--%s\n", token);
 			token = strtok(NULL, delimiters);
 		}
 	}
